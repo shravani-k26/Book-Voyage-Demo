9107c32ee7ed0641a6762e47ff95c7d7750243de
shravani-k26 <shravanikijbile@gmail.com>
2025-01-12 20:16:49 +0530

    Firebase Connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
-
 import 'package:flutter/material.dart';
 import 'package:book_voyage_demo/login.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
